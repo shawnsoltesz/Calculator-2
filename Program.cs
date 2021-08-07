a1b892eeb284e117137bbfb44fb69a652676a91e
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-06 21:51:33 -0400

    Converting and Adding integers and doubles

--- Program.cs
+++ Program.cs
@@ -2,11 +2,44 @@
 
 namespace Calculator
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //Convert string to integer
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num);
+
+            // string to integer and math function
+            Console.WriteLine(num + 6);
+
+            //Calculator
+
+            //Adding two integers
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            //Adding two doubles (due to decimals)
+            Console.Write("Enter a number: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a number: ");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num3 + num4);
+
+
+
+
+
+
+
+
+        }
     }
-  }
 }
