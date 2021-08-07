aaff8d2afea41b22dd3e377280bfc29c6ccaf594
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-06 22:57:02 -0400

    Methods

--- Program.cs
+++ Program.cs
@@ -4,89 +4,90 @@ namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        //New Method
+        static void SayHi(string name)
         {
-            /*
-            
-            //Convert string to integer
-            int num = Convert.ToInt32("45");
-            Console.WriteLine(num);
 
-            // string to integer and math function
-            Console.WriteLine(num + 6);
+        })
+        {
 
-            //Calculator
-
-            //Adding two integers
-            Console.Write("Enter a number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter a number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(num1 + num2);
-
-            //Adding two doubles (due to decimals)
-            Console.Write("Enter a number: ");
-            double num3 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter a number: ");
-            double num4 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine(num3 + num4);
-
-            
-            //Madlibs Game
-
-            string color, pluralNoun, celebrity;
-
-            Console.Write("Enter a color: ");
-            color = Console.ReadLine();
-
-            Console.Write("Enter a plural noun: ");
-            pluralNoun = Console.ReadLine();
-
-            Console.Write("Enter a celebrity: ");
-            celebrity = Console.ReadLine();
-
-            Console.WriteLine("Roses are " + color);
-            Console.WriteLine(pluralNoun + " are blue");
-            Console.WriteLine("I love " + celebrity);
-            
-            */
-
-            //Arrays
-
-            //identify a number at a specific index of an array
-            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
-            Console.WriteLine(luckyNumbers[4]);
-
-            //update a number in array by index number
-            //int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
-            luckyNumbers[1] = 900;
-            Console.WriteLine(luckyNumbers[1]);
-
-            //setting up an array with room for 5 elements. assigning values later by index
-            string[] friends = new string[5];
-            friends[0] = "Jim";
-            friends[1] = "Mandy";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("Hello + name");
 
         }
+
+
+    static void Main(string[] args)
+
+    {
+        SayHi(Mike);
+
+        /*
+
+        //Convert string to integer
+        int num = Convert.ToInt32("45");
+        Console.WriteLine(num);
+
+        // string to integer and math function
+        Console.WriteLine(num + 6);
+
+        //Calculator
+
+        //Adding two integers
+        Console.Write("Enter a number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter a number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2);
+
+        //Adding two doubles (due to decimals)
+        Console.Write("Enter a number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter a number: ");
+        double num4 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(num3 + num4);
+
+
+        //Madlibs Game
+
+        string color, pluralNoun, celebrity;
+
+        Console.Write("Enter a color: ");
+        color = Console.ReadLine();
+
+        Console.Write("Enter a plural noun: ");
+        pluralNoun = Console.ReadLine();
+
+        Console.Write("Enter a celebrity: ");
+        celebrity = Console.ReadLine();
+
+        Console.WriteLine("Roses are " + color);
+        Console.WriteLine(pluralNoun + " are blue");
+        Console.WriteLine("I love " + celebrity);
+
+
+
+        //Arrays
+
+        //identify a number at a specific index of an array
+        int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+        Console.WriteLine(luckyNumbers[4]);
+
+        //update a number in array by index number
+        //int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+        luckyNumbers[1] = 900;
+        Console.WriteLine(luckyNumbers[1]);
+
+        //setting up an array with room for 5 elements. assigning values later by index
+        string[] friends = new string[5];
+        friends[0] = "Jim";
+        friends[1] = "Mandy";
+
+        */
+
     }
+}
 }
